6efd78b619f59bf5e15d77be26767f8c7a3a7253
David Shivulu <119101128+David-Shivulu@users.noreply.github.com>
2025-05-02 15:09:41 +0300

    Delete BACKEND/api_and_endpoints.py

--- BACKEND/api_and_endpoints.py
+++
@@ -1,8 +0,0 @@
-# This script is responsible for handling all api keys and api endpoints...
-
-# ------------------------------------------- GOOGLE AUTH -----------------------------------------------
-CLIENT_ID = ""
-CLIENT_SK = ""
-
-# -------------------------------------------- OPENAI --------------------------------------------------
-OPENAI_API_KEY = ""
